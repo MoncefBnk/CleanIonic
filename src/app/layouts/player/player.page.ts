1a9b05f6294c5ef3fd6851f4a8811a2fa42aa6a4
Moncyyy <moncef-benkhalfallah@outlook.com>
2024-06-07 09:36:10 +0200

    WIP : Player range slider working according to song and timing added

--- src/app/layouts/player/player.page.ts
+++ src/app/layouts/player/player.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RangeCustomEvent } from '@ionic/angular';
+
 import {
   IonContent,
   IonHeader,
@@ -22,19 +22,12 @@ import {
   ellipsisHorizontal,
   expand,
   heart,
-  heartCircleOutline,
-  heartCircleSharp,
   heartOutline,
-  pause,
   pauseCircle,
-  play,
   playCircle,
-  playSkipBack,
   playSkipBackOutline,
-  playSkipForward,
   playSkipForwardOutline,
   repeat,
-  repeatOutline,
   shareOutline,
   shareSocialOutline,
   shuffle,
@@ -65,6 +58,27 @@ import {
   ],
 })
 export class PlayerPage implements OnInit {
+  @ViewChild('audioPlayer', { static: false }) audioPlayer: any;
+  audio!: HTMLAudioElement;
+  isFavorite: boolean = false;
+  isPlaying: boolean = true;
+  currentTime: string = '0:00';
+  duration: string = '0:00';
+  progress: number = 0;
+  lyrics: string[] = [
+    "...la-la-la-la-la-la-la, la, la La-la-la-la-la-la-la-la la la-la",
+    "Now that I've lost everything to you You say you wanna start something new And it's breakin' my heart you're leavin' Baby, I'm grievin' But if you wanna leave, take good care Hope you have a lot of nice things to wear But then a lot of nice things turn bad out there",
+    "Oh, baby, baby, it's a wild world It's hard to get by just upon a smile Oh, baby, baby, it's a wild world I'll always remember you like a child, girl ",
+    "You know I've seen a lot of what the world can do And it's breakin' my heart in two Because I never wanna see you sad, girl Don't be a bad girl But if you wanna leave, take good care Hope you make a lot of nice friends out there But just remember there's a lot of bad and beware",
+    "Oh, baby, baby, it's a wild world And it's hard to get by just upon a smile Oh, baby, baby, it's a wild world And I'll always remember you like a child, girl",
+    " ...la-la-la-la-la-la-la, la, la La-la-la-la-la-la-la-la la la-la, la Baby, I love you But if you wanna leave, take good care Hope you make a lot of nice friends out there But just remember there's a lot of bad and beware Beware",
+    "Oh, baby, baby, it's a wild world It's hard to get by just upon a smile Oh, baby, baby, it's a wild world And I'll always remember you like a child, girl Oh, baby, baby, it's a wild world And it's hard to get by just upon a smile Oh, baby, baby, it's a wild world And I'll always remember you like a child, girl",
+  ];
+  currentLyric: string = 'No Lyrics Found';
+  start_icon: string = 'search';
+  end_icon: string = 'search';
+  image: string = 'assets/icon/logo_mini.png';
+
   constructor() {
     addIcons({
       repeat,
@@ -82,33 +96,47 @@ export class PlayerPage implements OnInit {
     });
   }
 
-  isFavorite: boolean = false;
-  isPlaying: boolean = true;
-  lyrics: string[] = [
-    "Now that I've lost everything to you You say you wanna start something new And it's breakin' my heart you're leavin' Baby, I'm grievin' But if you wanna leave, take good care Hope you have a lot of nice things to wear But then a lot of nice things turn bad out there",
-    "Oh, baby, baby, it's a wild world It's hard to get by just upon a smile Oh, baby, baby, it's a wild world I'll always remember you like a child, girl ",
-    "You know I've seen a lot of what the world can do And it's breakin' my heart in two Because I never wanna see you sad, girl Don't be a bad girl But if you wanna leave, take good care Hope you make a lot of nice friends out there But just remember there's a lot of bad and beware",
-    "Oh, baby, baby, it's a wild world And it's hard to get by just upon a smile Oh, baby, baby, it's a wild world And I'll always remember you like a child, girl",
-    " ...la-la-la-la-la-la-la, la, la La-la-la-la-la-la-la-la la la-la, la Baby, I love you But if you wanna leave, take good care Hope you make a lot of nice friends out there But just remember there's a lot of bad and beware Beware",
-    "Oh, baby, baby, it's a wild world It's hard to get by just upon a smile Oh, baby, baby, it's a wild world And I'll always remember you like a child, girl Oh, baby, baby, it's a wild world And it's hard to get by just upon a smile Oh, baby, baby, it's a wild world And I'll always remember you like a child, girl",
-  ];
-  currentLyric: string = 'No Lyrics Found';
-  start_icon: string = 'search';
-  end_icon: string = 'search';
-  image: string = 'assets/icon/logo_mini.png';
+  ngOnInit() {
+    this.audio = new Audio('assets/songs/Wild_World.mp3');
+    this.audio.addEventListener('timeupdate', this.updateProgress.bind(this));
+    this.audio.addEventListener('loadedmetadata', () => {
+      this.duration = this.formatTime(this.audio.duration);
+    });
+  }
 
   playMusic() {
     this.isPlaying = !this.isPlaying;
+    if (this.isPlaying) {
+      this.audio.play();
+    } else {
+      this.audio.pause();
+    }
+  }
+
+  updateProgress() {
+    this.progress = (this.audio.currentTime / this.audio.duration) * 100;
+    this.currentTime = this.formatTime(this.audio.currentTime);
+    this.currentLyric = this.getLyric(this.progress);
   }
 
   onIonChange(event: any) {
     const value = event.detail.value;
-    const lyricIndex = Math.floor(value / (100 / this.lyrics.length));
-    this.currentLyric = this.lyrics[lyricIndex];
+    const seekTime = (value / 100) * this.audio.duration;
+    this.audio.currentTime = seekTime;
   }
 
   makeFavorite() {
     this.isFavorite = !this.isFavorite;
   }
-  ngOnInit() {}
+
+  getLyric(progress: number): string {
+    const lyricIndex = Math.floor((progress / 100) * this.lyrics.length);
+    return this.lyrics[lyricIndex] || 'No Lyrics Found';
+  }
+
+  formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+  }
 }
